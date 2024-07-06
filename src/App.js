675655803c34a7cb75f4165fc205f0445492194b
akkis9090 <akshaysutar9090@gmail.com>
2024-07-06 22:31:55 +0530

    error page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Products from './Products';
+import Contact from './Contact';
+import SingleProduct from './SingleProduct';
+import Cart from './Cart';
+import ErrorPage from './ErrorPage';
 
 const App = () => {
-  return <div>Welcome to AR React E-Commerce Website</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/singleProduct/:id' element={<SingleProduct />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='*' element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
--- src/ErrorPage.js
+++ src/ErrorPage.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const ErrorPage = () => {
     return (
         <div>
-            Home
+            Page Not Found
         </div>
     );
 };
 
-export default Home;
+export default ErrorPage;
